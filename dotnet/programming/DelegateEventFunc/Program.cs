e098d705396e65c4ac354075890c05b1ac9670c7
Masum Billah <mdkmscsepust@gmail.com>
2025-07-30 05:14:56 +0000

    add-eventdriven-syncasyncdemo

---
+++ dotnet/programming/DelegateEventFunc/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+// ...existing code...
+var service = new ServiceCollection();
+service.AddScoped<OrderService>();
+service.AddScoped<NotificationService>();
+var provider = service.BuildServiceProvider();
+
+var orderService = provider.GetRequiredService<OrderService>();
+var notificationService = provider.GetRequiredService<NotificationService>();
+orderService.OrderPlaced += notificationService.OnOrderPlaced;
+orderService.PlacedOrder(1, "masumcsepust@gmail.com");
+public class OrderEventArgs : EventArgs
+{
+    public int OrderId { get; }
+    public string Username { get; }
+    public OrderEventArgs(int orderId, string username)
+    {
+        OrderId = orderId;
+        Username = username;
+    }
+}
+
+public class OrderService
+{
+    public event EventHandler<OrderEventArgs> OrderPlaced;
+    public void PlacedOrder(int orderId, string username)
+    {
+        OrderPlaced?.Invoke(this, new OrderEventArgs(orderId, username));
+    }
+}
+
+public class NotificationService
+{
+    public void OnOrderPlaced(object sender, OrderEventArgs e)
+    {
+        Console.WriteLine($"Notification Order Placed: OrderId = {e.OrderId} Username = {e.Username}");
+    }
+}
---
+++ dotnet/programming/SyncAsyncDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿var time = DateTime.Now;
+CPUBoundExample();
+var endtime = DateTime.Now - time;
+Console.WriteLine($"Time taken: {endtime.TotalMilliseconds} ms");
+var ptime = DateTime.Now;
+ParallelForEachExample();
+var pendtime = DateTime.Now - ptime;
+Console.WriteLine($"Parallel Time taken: {pendtime.TotalMilliseconds} ms");
+static void CPUBoundExample()
+{
+    Console.WriteLine("Cpu bound Example");
+    long sum = 0;
+    for (int i = 0; i < 10000000; i++)
+    {
+        sum += i;
+    }
+    Console.WriteLine($"Sum: {sum}");
+}
+
+static void ParallelForEachExample()
+{
+    Console.WriteLine("Parallel Foreach Example");
+    long sum = 0;
+    Parallel.ForEach(Enumerable.Range(0, 10000000), i =>
+    {
+       Interlocked.Add(ref sum, i);
+    });
+
+    Console.WriteLine($"Sum: {sum}");
+}
